7a15a49f6c495110eaabc3ad94871a3c86b94191
Szilvia Mezo <Szilvia.Mezo@ibm.com>
2021-03-29 17:02:27 +0200

    Guardian 2 refactored

--- tictactoekata.js
+++ tictactoekata.js
@@ -1,8 +1,30 @@
 const tictactoekata = () => {
     let result = "Game board creation...";
     result += "\n";
+    let table = [[" "," "," "],[" "," "," "],[" "," "," "]];
+    result += drawTable(table);
+    result += "Board Created."
     result += "The game will start with Player X\n";
     return result;
 };
+
+const drawTable = (table) => {
+    let drawnTable = "";
+    for(let indexY = 0; indexY < 3; indexY++) {
+        for(let indexX = 0; indexX < 3; indexX++) {
+          drawnTable += table[indexY][indexX];
+          if(indexX < 2) {
+              drawnTable += "|";
+          } else {
+              drawnTable += "\n";
+          }
+        }
+        if(indexY < 2) {
+            drawnTable += "-+-+-\n"
+        }
+    }
+    return drawnTable;
+}
+
 
 module.exports =  { tictactoekata }
