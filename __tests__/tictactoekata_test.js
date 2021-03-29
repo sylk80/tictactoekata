3ab223573115dada4c4f75df3988a4fc84a7b21f
Szilvia Mezo <Szilvia.Mezo@ibm.com>
2021-03-29 17:13:15 +0200

    refactor goals

--- __tests__/tictactoekata_test.js
+++ __tests__/tictactoekata_test.js
@@ -2,8 +2,8 @@ const  { tictactoekata } = require('../tictactoekata');
 
 
 describe('Tic-tact-toe game should be...', function() {
-describe('where the following rules are true, that..', function() {
-    it('played by Player X and O', function() {
+describe('where the following should apply, that..', function() {
+    it('the game is played by Player X and O', function() {
         let result = tictactoekata();
         expect(result).toContain("Player X");
     });
